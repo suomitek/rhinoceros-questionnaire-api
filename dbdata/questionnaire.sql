701d802973333c55b15cf7fb15541ebb11c93383
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-09-19 00:10:34 +0800

    update database

--- dbdata/questionnaire.sql
+++ dbdata/questionnaire.sql
@@ -17,6 +17,8 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+use rhinoceros;
+
 -- ----------------------------
 -- Table structure for admin
 -- ----------------------------
@@ -25,14 +27,17 @@ CREATE TABLE `admin` (
   `admin_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '管理员ID',
   `a_username` varchar(25) NOT NULL COMMENT '用户名',
   `a_password` varchar(40) NOT NULL COMMENT '密码',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '状态',
+  `create_time` bigint(20) NOT NULL default '0' COMMENT '创建时间',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
   PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='管理员表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='管理员表';
 
 -- ----------------------------
 -- Records of admin
 -- ----------------------------
 BEGIN;
-INSERT INTO `admin` VALUES (1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997');
+INSERT INTO `admin` VALUES (1, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997',1,unix_timestamp(now()),unix_timestamp(now()));
 COMMIT;
 
 -- ----------------------------
@@ -46,8 +51,11 @@ CREATE TABLE `ads` (
   `ad_url` varchar(255) NOT NULL COMMENT '广告图片地址',
   `ad_href` varchar(255) NOT NULL COMMENT '广告外链',
   `ad_target` int(1) NOT NULL COMMENT '广告打开方式，0本窗口，1新窗口',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '状态',
+  `create_time` bigint(20) NOT NULL default '0' COMMENT '创建时间',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
   PRIMARY KEY (`ad_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='广告表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='广告表';
 
 -- ----------------------------
 -- Table structure for configs
@@ -58,14 +66,17 @@ CREATE TABLE `configs` (
   `config_name` varchar(255) NOT NULL COMMENT '配置名称',
   `config_val` text NOT NULL COMMENT '配置的值',
   `config_comments` text NOT NULL COMMENT '配置说明',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '状态',
+  `create_time` bigint(20) NOT NULL default '0' COMMENT '创建时间',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
   PRIMARY KEY (`config_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of configs
 -- ----------------------------
 BEGIN;
-INSERT INTO `configs` VALUES (3, 'smtp_sending', '0', '0未在任务中，1在任务中');
+INSERT INTO `configs` VALUES (1, 'smtp_sending', '0', '0未在任务中，1在任务中',1,unix_timestamp(now()),unix_timestamp(now()));
 COMMIT;
 
 -- ----------------------------
@@ -75,15 +86,17 @@ DROP TABLE IF EXISTS `naire`;
 CREATE TABLE `naire` (
   `naire_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '问卷id',
   `admin_id` bigint(20) NOT NULL COMMENT '管理员id',
-  `n_createtime` varchar(14) NOT NULL COMMENT '创建时间',
+  `n_createtime` bigint(20) NOT NULL COMMENT '创建时间',
   `n_deadline` varchar(14) NOT NULL COMMENT '截止时间',
   `n_title` varchar(255) NOT NULL COMMENT '问卷标题',
   `n_status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '发布状态',
   `n_intro` text COMMENT '问卷介绍',
   `n_options` text NOT NULL COMMENT '问卷相关配置(JSON)',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '状态',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
   PRIMARY KEY (`naire_id`),
   KEY `naire_id` (`naire_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8 COMMENT='问卷表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='问卷表';
 
 -- ----------------------------
 -- Table structure for options
@@ -97,9 +110,12 @@ CREATE TABLE `options` (
   `naire_id` bigint(20) NOT NULL COMMENT '问卷ID',
   `question_id` bigint(20) NOT NULL COMMENT '题目ID',
   `o_isaddtion` tinyint(1) NOT NULL DEFAULT '0' COMMENT '是否有附加内容',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '状态',
+  `create_time` bigint(20) NOT NULL default '0' COMMENT '创建时间',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
   PRIMARY KEY (`options_id`),
   KEY `options_id` (`options_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=792 DEFAULT CHARSET=utf8 COMMENT='题目选项表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='题目选项表';
 
 -- ----------------------------
 -- Table structure for question
@@ -113,20 +129,28 @@ CREATE TABLE `question` (
   `q_isrequire` tinyint(1) NOT NULL COMMENT '是否为必填项',
   `q_setting` varchar(500) NOT NULL COMMENT '题目配置，如至少选几项等',
   `q_description` text COMMENT '问题描述',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '状态',
+  `create_time` bigint(20) NOT NULL default '0' COMMENT '创建时间',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
   PRIMARY KEY (`question_id`),
   KEY `question_id` (`question_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=168 DEFAULT CHARSET=utf8 COMMENT='题目表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='题目表';
 
 -- ----------------------------
 -- Table structure for result
 -- ----------------------------
 DROP TABLE IF EXISTS `result`;
 CREATE TABLE `result` (
+  `result_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'result_id',
   `user_id` bigint(20) NOT NULL COMMENT '用户ID',
   `naire_id` bigint(20) NOT NULL COMMENT '问卷ID',
   `question_id` bigint(20) NOT NULL COMMENT '题目ID',
   `options_id` bigint(20) NOT NULL COMMENT '选项ID',
   `o_addtion` text COMMENT '附加文字',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '状态',
+  `create_time` bigint(20) NOT NULL default '0' COMMENT '创建时间',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
+  PRIMARY KEY (`result_id`),
   UNIQUE KEY `user_id` (`user_id`,`naire_id`,`question_id`,`options_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -134,11 +158,14 @@ CREATE TABLE `result` (
 -- Table structure for submit_log
 -- ----------------------------
 DROP TABLE IF EXISTS `submit_log`;
+-- truncate table submit_log;
 CREATE TABLE `submit_log` (
   `submit_log_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '填写时间表',
   `naire_id` bigint(20) NOT NULL COMMENT '问卷ID',
   `user_id` bigint(20) NOT NULL COMMENT '用户ID',
-  `s_createtime` varchar(14) NOT NULL COMMENT '完成时间',
+  `s_createtime` bigint(20) NOT NULL COMMENT '完成时间',
+  `status` tinyint(4) NOT NULL default '0' COMMENT '状态',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
   PRIMARY KEY (`submit_log_id`),
   UNIQUE KEY `naire_id` (`naire_id`,`user_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -152,8 +179,8 @@ CREATE TABLE `task` (
   `user_id` bigint(20) NOT NULL COMMENT '用户ID',
   `naire_id` bigint(20) NOT NULL COMMENT '问卷ID',
   `task_status` int(1) NOT NULL DEFAULT '0' COMMENT '发送状态（0未发，1已发，2处理中）',
-  `task_createtime` varchar(14) NOT NULL COMMENT '任务创建时间',
-  `task_updatetime` varchar(14) NOT NULL COMMENT '任务状态更新时间',
+  `task_createtime` bigint(20) NOT NULL COMMENT '任务创建时间',
+  `task_updatetime` bigint(20) NOT NULL COMMENT '任务状态更新时间',
   PRIMARY KEY (`task_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -174,9 +201,11 @@ CREATE TABLE `users` (
   `u_tel` varchar(11) NOT NULL COMMENT '手机号',
   `u_email` varchar(255) NOT NULL COMMENT '邮箱',
   `u_status` int(1) NOT NULL DEFAULT '0' COMMENT '用户状态（0正常，1被冻结）',
-  `u_active_time` varchar(14) NOT NULL COMMENT '用户最近的活跃时间',
+  `u_active_time` bigint(20) NOT NULL COMMENT '用户最近的活跃时间',
+  `create_time` bigint(20) NOT NULL default '0' COMMENT '创建时间',
+  `update_time` bigint(20) NOT NULL default '0' COMMENT '更新时间',
   PRIMARY KEY (`user_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21073 DEFAULT CHARSET=utf8 COMMENT='用户表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 SET FOREIGN_KEY_CHECKS = 1;
